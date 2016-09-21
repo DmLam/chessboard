b396e132b514d53f24290b3a8f5dfc5b43c14028
Dmitry Lamdan <lamdan@mail.ru>
2016-09-21 17:05:11 +0300

    21-09-2016 w

---
+++ src/main/java/dmlam/ru/chessboard/PGNError.java
@@ -0,0 +1,12 @@
+package dmlam.ru.chessboard;
+
+/**
+ * Created by LamdanDY on 21.09.2016.
+ */
+
+public class PGNError extends Exception{
+    public PGNError(String message) {
+        super(message);
+    }
+};
+
