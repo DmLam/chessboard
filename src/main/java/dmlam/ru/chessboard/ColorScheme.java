8caead1de3f678ae6dcbde897df2fbe6926bbdf4
Dmitry Lamdan <lamdan@mail.ru>
2017-03-22 16:33:15 +0300

    Переименование colorScheme в ColorScheme

---
+++ src/main/java/dmlam/ru/chessboard/ColorScheme.java
@@ -0,0 +1,33 @@
+package dmlam.ru.chessboard;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by LamdanDY on 07.10.2016.
+ */
+
+public abstract class ColorScheme {
+    private String name;
+    protected ChessBoardView chessBoardView;
+
+    public ColorScheme(String name, ChessBoardView chessBoardView) {
+        this.chessBoardView = chessBoardView;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    abstract protected void drawBoard(Canvas canvas);
+
+    abstract protected void initPainting();
+
+    abstract protected void donePainting();
+
+    abstract protected void updatePainters(float cellSize, float textSize);
+
+    abstract public int getWhiteColor();
+
+    abstract public int getBlackColor();
+}
