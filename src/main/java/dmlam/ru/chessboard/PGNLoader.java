68b0cebbe3406a7c993b59ed91d701eb7137fa20
LamdanDY <LamdanDY@yanos.slavneft.ru>
2018-10-01 11:57:26 +0300

    PGNLoader can load a game from string

--- src/main/java/dmlam/ru/chessboard/PGNLoader.java
+++ src/main/java/dmlam/ru/chessboard/PGNLoader.java
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 
 import dmlam.ru.androidcommonlib.FileNameUtils;
@@ -65,27 +66,23 @@ public class PGNLoader {
                             game.addTag(name, value);
                             result = true;
                         }
-                    }
-                    else {
+                    } else {
                         throw new PGNError("Wrong PGN format (no space after tag name)");
                     }
-                }
-                else {
+                } else {
                     throw new PGNError("Unexpected end of line");
                 }
-            }
-            else {
+            } else {
                 throw new PGNError("Unexpected end of file: no moves section");
             }
-        }
-        else {
+        } else {
             in.reset();
         }
 
         return result;
     }
 
-    private boolean readTags(BufferedReader in, Game game) throws PGNError, IOException  {
+    private boolean readTags(BufferedReader in, Game game) throws PGNError, IOException {
         boolean result = false;
 
         while (readTag(in, game)) {
@@ -99,7 +96,7 @@ public class PGNLoader {
     private void skipSpace(StringBuilder moves) {
         int i;
 
-        for (i = 0; i < moves.length() && moves.charAt(i) == ' '; i++);
+        for (i = 0; i < moves.length() && moves.charAt(i) == ' '; i++) ;
         if (i > 0) {
             moves.delete(0, i);
         }
@@ -135,8 +132,7 @@ public class PGNLoader {
                 if (commentEnd > 0) {
                     result = moves.substring(1, commentEnd);
                     moves.delete(0, commentEnd + 1);
-                }
-                else {
+                } else {
                     throw new PGNError("End of comment not found");
                 }
             }
@@ -159,13 +155,10 @@ public class PGNLoader {
             if (!Character.isDigit(c)) {
                 if (chessboard.getLastMove() == null) {
                     moveNumber = 1;
-                }
-                else {
+                } else {
                     moveNumber = chessboard.getLastMove().getMoveNumber();
                 }
-            }
-            else
-            {
+            } else {
                 StringBuilder num = new StringBuilder();
 
                 num.append(c);
@@ -188,17 +181,14 @@ public class PGNLoader {
                     if (moves.length() > 2 && moves.charAt(1) == '.' && moves.charAt(2) == '.') {
                         moveOrder = BLACK;
                         moves.delete(0, 3);
-                    }
-                    else {
+                    } else {
                         moveOrder = WHITE;
                         moves.delete(0, 1);
                     }
-                }
-                else {
+                } else {
                     throw new PGNError("Incorrect move number");
                 }
-            }
-            else {
+            } else {
                 moveOrder = chessboard.getLastMove().getMoveOrder();
             }
 
@@ -242,18 +232,14 @@ public class PGNLoader {
 
                     if (moves.charAt(0) == '*') {
                         r = UNKNOWN;
-                    }
-                    else
-                    if (moves.length() > 2) {
+                    } else if (moves.length() > 2) {
                         String s = moves.substring(0, 3);
 
                         if (s.equals("1/2")) {
                             r = DRAW;
-                        } else
-                        if (s.equals("1-0")) {
+                        } else if (s.equals("1-0")) {
                             r = Game.GameResult.WHITE;
-                        } else
-                        if (s.equals("0-1")) {
+                        } else if (s.equals("0-1")) {
                             r = Game.GameResult.BLACK;
                         }
                     }
@@ -281,13 +267,9 @@ public class PGNLoader {
                     if (moves.charAt(0) == '{') {
                         chessboard.getLastMove().setComment(parseComment(moves));
                         skipSpace(moves);
-                    }
-                    else
-                    if (moves.charAt(0) == '$') {
+                    } else if (moves.charAt(0) == '$') {
                         chessboard.getLastMove().setNumericAnnotationGlyph(parseNumericAnnotationGlyph(moves));
-                    }
-                    else
-                    if (moves.charAt(0) == '(') {
+                    } else if (moves.charAt(0) == '(') {
                         // variant
                         int lastMoveId = chessboard.getLastMove().getMoveId();
 
@@ -300,17 +282,14 @@ public class PGNLoader {
                             // after a variant can be only another variant or earlier variant ending
                             break;
                         }
-                    }
-                    else
-                    if (moves.charAt(0) == ')') {
+                    } else if (moves.charAt(0) == ')') {
                         // end of variant
                         moves.delete(0, 1);
                         skipSpace(moves);
                         chessboard.moveVariantDown(variantStart);
 
                         return true;
-                    }
-                    else {
+                    } else {
                         break;
                     }
 
@@ -355,12 +334,13 @@ public class PGNLoader {
         return result;
     }
 
-    private Game readGame(BufferedReader in) throws PGNError, IOException  {
+    private Game readGame(BufferedReader in) throws PGNError, IOException {
         Game result = new Game();
 
         if (!readTags(in, result) || !readMoves(in, result)) {
             result = null;
-        };
+        }
+        ;
 
         return result;
     }
@@ -371,8 +351,7 @@ public class PGNLoader {
 
         try {
             fr = new FileReader(fileName);
-        }
-        catch(FileNotFoundException E) {
+        } catch (FileNotFoundException E) {
             Log.e(LOGTAG, String.format(LOGTAG + " PGN file not found (%s)", fileName));
 
             throw new PGNError(String.format("PGN file not found (%s)\n%s", fileName, E.toString()));
@@ -387,8 +366,7 @@ public class PGNLoader {
             }
 
             fos = new FileOutputStream(f);
-        }
-        catch(IOException E) {
+        } catch (IOException E) {
             Log.e(LOGTAG, String.format(LOGTAG + " PGN file not found (%s)", fileName));
 
             throw new PGNError(String.format("Can't create pgn index (%s)", fileName));
@@ -396,6 +374,7 @@ public class PGNLoader {
 
         BufferedReader in = new BufferedReader(fr);
 
+        gamesIndex.clear();
         gamesIndex.add(0); // index of the first game
         try {
             int gameNo = 1;
@@ -404,17 +383,16 @@ public class PGNLoader {
                     while (readGame(in) != null) {
                         gamesIndex.add(lineNum);
                         gameNo++;
-                    };
+                    }
+                    ;
                     gamesIndex.remove(gamesIndex.size() - 1);
-                }
-                catch (Exception E) {
+                } catch (Exception E) {
                     Log.e(LOGTAG, String.format(LOGTAG + "Error reading PGN file %s (game %d, line %d):\n%s", fileName, gameNo, lineNum, E.getMessage()));
                 }
             } finally {
                 in.close();
             }
-        }
-        catch (IOException E) {
+        } catch (IOException E) {
             Log.e(LOGTAG, String.format(LOGTAG + " Error reading file %s:\n%s", fileName, E.toString()));
         }
 
@@ -422,16 +400,19 @@ public class PGNLoader {
 
         for (int i = 0; i < gamesIndex.size(); i++) {
             int index = gamesIndex.get(i);
-            buf[i * 4] = (byte) index; index /= 256;
-            buf[i * 4 + 1] = (byte) index; index /= 256;
-            buf[i * 4 + 2] = (byte) index; index /= 256;
+            buf[i * 4] = (byte) index;
+            index /= 256;
+            buf[i * 4 + 1] = (byte) index;
+            index /= 256;
+            buf[i * 4 + 2] = (byte) index;
+            index /= 256;
             buf[i * 4 + 3] = (byte) index;
         }
 
         try {
             fos.write(buf);
             fos.close();
-        }catch (IOException E) {
+        } catch (IOException E) {
             Log.e(LOGTAG, String.format(LOGTAG + "Error writing index %s :\n%s", indexFileName, E.getMessage()));
 
             throw new PGNError(String.format("Error creating pgn index (%s)", fileName));
@@ -459,44 +440,45 @@ public class PGNLoader {
             fis = new FileInputStream(f);
 
             fis.read(buf);
-        }
-        catch(FileNotFoundException E) {
+        } catch (FileNotFoundException E) {
             Log.e(LOGTAG, String.format(LOGTAG + " PGN index file not found (%s)", indexFileName));
             throw new PGNError(String.format("PGN index file not found (%s)\n%s", indexFileName, E.toString()));
-        }
-        catch(IOException E) {
+        } catch (IOException E) {
             Log.e(LOGTAG, String.format(LOGTAG + " Error reading PGN index file (%s)", indexFileName));
             throw new PGNError(String.format("Error reading PGN index file (%s)\n%s", indexFileName, E.toString()));
         }
 
         gamesIndex.clear();
         for (int i = 0; i < fileSize; i += 4) {
-            int idx = bytes2int(buf[i], buf[i+1], buf[i+2], buf[i+3]);
+            int idx = bytes2int(buf[i], buf[i + 1], buf[i + 2], buf[i + 3]);
 
             gamesIndex.add(idx);
         }
     }
 
-    public PGNLoader(String fileName) throws PGNError {
+    private void updateIndexFile(String fileName) throws PGNError {
         String indexFileName = FileNameUtils.changeExtension(fileName, "gamesIndex");
         File f = new File(indexFileName);
 
         if (!f.exists()) {
             createFileIndex(fileName, indexFileName);
-        }
-        else {
+        } else {
             loadIndexFile(indexFileName);
         }
-
-        this.fileName = fileName;
     }
 
     public int getGamesCount() {
         return gamesIndex.size();
     }
 
-    public Game getGame(int index) throws PGNError{
+    public Game getGame(String fileName, int index) throws PGNError {
         Game result;
+
+        if (!fileName.equals(this.fileName)) {
+            updateIndexFile(fileName);
+
+            this.fileName = fileName;
+        }
 
         try {
             FileReader fr = new FileReader(fileName);
@@ -507,16 +489,34 @@ public class PGNLoader {
             }
 
             result = readGame(reader);
-        }
-        catch(IOException E) {
+        } catch (IOException E) {
             Log.e(LOGTAG, String.format(LOGTAG + "Error reading PGN file %s (game %d, line %d):\n%s", fileName, index, lineNum, E.toString()));
 
             throw new PGNError(String.format(LOGTAG + "Error reading PGN file %s (game %d, line %d, message '%s')", fileName, index, lineNum, E.getMessage()));
-        }
-        catch(PGNError E) {
+        } catch (PGNError E) {
             Log.e(LOGTAG, String.format(LOGTAG + "Error reading PGN file %s (game %d, line %d):\n%s", fileName, index, lineNum, E.toString()));
 
             throw new PGNError(String.format(LOGTAG + "Error reading PGN file %s (game %d, line %d)", fileName, index, lineNum));
+        }
+
+        return result;
+    }
+
+    public Game getGame(String moves) throws PGNError {
+        Game result;
+        StringReader stringReader = new StringReader(moves);
+        BufferedReader reader = new BufferedReader(stringReader);
+
+        try {
+            result = readGame(reader);
+        } catch (IOException E) {
+            Log.e(LOGTAG, String.format(LOGTAG + "Error reading PGN moves (%s, line %d): %s", moves, lineNum, E.toString()));
+
+            throw new PGNError(String.format(LOGTAG + "Error reading PGN file %s (%s, line %d): %s", moves, lineNum, E.getMessage()));
+        } catch (PGNError E) {
+            Log.e(LOGTAG, String.format(LOGTAG + "Error reading PGN moves (%s, line %d): %s", moves, lineNum, E.toString()));
+
+            throw new PGNError(String.format(LOGTAG + "Error reading PGN file %s (%s, line %d): %s", moves, lineNum, E.getMessage()));
         }
 
         return result;
